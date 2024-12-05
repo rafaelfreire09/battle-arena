01298573aa4b1be189031f9a8461bbfd9d1d5a1e
Rafael Freire <rafaeldacostafreire@gmail.com>
2024-12-05 09:17:14 -0300

    refactor: :fire: page 'Game' deleted

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { GameMatch } from "./pages/GameMatch";
 import Lobby from "./pages/Lobby";
 import Login from "./pages/Login";
-import Game from "./pages/Game";
 
 const App = () => {
   return (
@@ -12,7 +11,6 @@ const App = () => {
           <Route path="/" element={<Login />} />
           <Route path="/game-match" element={<GameMatch />} />
           <Route path="/lobby" element={<Lobby />} />
-          <Route path="/game" element={<Game />} />
         </Routes>
       </BrowserRouter>
     </>
